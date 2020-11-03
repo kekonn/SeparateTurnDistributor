504e122d9f535e37cb3dbaf9426f8d228fe3173f
Jonas Van der Aa <ketsukonn@gmail.com>
2020-11-03 22:32:54 +0100

    Add Test project for SyncEngine

---
+++ ChessClock.SyncEngine.Tests/DefaultAutoSyncStrategyTests.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace ChessClock.SyncEngine.Tests
+{
+    public class DefaultAutoSyncStrategyTests
+    {
+
+        [Fact]
+        public void StrategyRespectsCooldown()
+        {
+
+        }
+    }
+}
