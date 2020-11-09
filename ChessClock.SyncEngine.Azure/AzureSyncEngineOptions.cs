04e6eb8bdb0e24ade6b9f3a1bd5b00ee48ca6399
Jonas Van der Aa <ketsukonn@gmail.com>
2020-11-09 23:40:08 +0100

    Create AzureSyncEngineOptions.cs

---
+++ ChessClock.SyncEngine.Azure/AzureSyncEngineOptions.cs
@@ -0,0 +1,12 @@
+﻿using ChessClock.Model;
+
+namespace ChessClock.SyncEngine.Azure
+{
+    public class AzureSyncEngineOptions
+    {
+        public string ConnectionString { get; set; }
+        public string TableName { get; set; }
+        public string ContainerName { get; set; }
+        public Player SystemPlayer { get; set; }
+    }
+}
