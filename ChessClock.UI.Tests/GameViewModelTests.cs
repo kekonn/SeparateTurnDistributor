3a33f4cc569d6dfed160986628a8853de8700750
Jonas Van der Aa <ketsukonn@gmail.com>
2020-12-04 11:18:54 +0100

    Add ViewModel Test Project

---
+++ ChessClock.UI.Tests/GameViewModelTests.cs
@@ -0,0 +1,29 @@
+using System;
+using ChessClock.SyncEngine;
+using Microsoft.Extensions.Logging;
+using Xunit;
+
+namespace ChessClock.UI.Tests
+{
+    public class GameViewModelTests
+    {
+        private ISyncEngine mockSyncEngine;
+        private static ILogger logger;
+
+        static GameViewModelTests()
+        {
+            logger = 
+        }
+
+        private GameViewModelTests()
+        {
+            mockSyncEngine = 
+        }
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ ChessClock.UI.Tests/SyncEngineTestImpl.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChessClock.Model;
+using ChessClock.SyncEngine;
+using Microsoft.Extensions.Logging;
+
+namespace ChessClock.UI.Tests
+{
+    internal class SyncEngineTestImpl : BaseSyncEngine
+    {
+        public SyncEngineTestImpl(Player player, ILogger<ISyncEngine> logger) : base(player, null, logger)
+        {
+        }
+
+        protected override void Sync(Game game)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ValueTask SubmitTurnAsync(Game game)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ValueTask PassTurnAsync(Game game)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IQueryable<Game> CreateGameSource()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override DateTimeOffset GetGameLastModifiedTime(Game game)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override DateTimeOffset GetRemoteSavefileLastModifiedTime(Game game)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override DateTimeOffset GetLocalSavefileLastModifiedTime(Game game)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
