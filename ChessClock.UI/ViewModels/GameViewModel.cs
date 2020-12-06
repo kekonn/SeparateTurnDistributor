4bff2eec9b7844992869fa0cc37677f4a8cd9fe2
Jonas Van der Aa <ketsukonn@gmail.com>
2020-12-06 10:55:02 +0100

    UI: Create initial game view and game viewmodel

---
+++ ChessClock.UI/ViewModels/GameViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessClock.UI.ViewModels
+{
+    public class GameViewModel : BaseViewModel
+    {
+        public override void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ValueTask InitializeAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
